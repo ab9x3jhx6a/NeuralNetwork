50f932f45aa96633d76762d5946307f35d8b02c0
Shanye <Jiang>
2015-12-08 18:13:01 -0500

    main framework

---
+++ Assets/Scripts/Motor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newpos = transform.position + Vector3.left/100;
+		transform.position = newpos;
+	}
+}
---
+++ Assets/Scripts/hit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Checkpoint") {
+			Destroy (other.gameObject);
+		}
+		Debug.Log ("hit");
+	}
+}
